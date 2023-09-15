b5af27b229bfc092aa78c4bcdc1070ef1573e9d6
--replace-all <quidev2505@gmail.com>
2023-09-15 16:55:39 +0700

    finish search function

--- frontend/src/Pages/Admin/PagesAdmin/Service/ServiceAdmin.jsx
+++ frontend/src/Pages/Admin/PagesAdmin/Service/ServiceAdmin.jsx
@@ -20,6 +20,7 @@ import {
   FolderViewOutlined,
   DeleteOutlined,
   SwapOutlined,
+  SearchOutlined,
 } from "@ant-design/icons";
 
 import axios from "axios";
@@ -142,6 +143,44 @@ function ServiceAdmin() {
     },
   ];
 
+  //Search Realtime
+  const [search, setSearch] = useState("");
+  useEffect(() => {
+    get_service();
+  }, [search]);
+
+  function removeVietnameseTones(str) {
+    str = str.replace(/à|á|ạ|ả|ã|â|ầ|ấ|ậ|ẩ|ẫ|ă|ằ|ắ|ặ|ẳ|ẵ/g, "a");
+    str = str.replace(/è|é|ẹ|ẻ|ẽ|ê|ề|ế|ệ|ể|ễ/g, "e");
+    str = str.replace(/ì|í|ị|ỉ|ĩ/g, "i");
+    str = str.replace(/ò|ó|ọ|ỏ|õ|ô|ồ|ố|ộ|ổ|ỗ|ơ|ờ|ớ|ợ|ở|ỡ/g, "o");
+    str = str.replace(/ù|ú|ụ|ủ|ũ|ư|ừ|ứ|ự|ử|ữ/g, "u");
+    str = str.replace(/ỳ|ý|ỵ|ỷ|ỹ/g, "y");
+    str = str.replace(/đ/g, "d");
+    str = str.replace(/À|Á|Ạ|Ả|Ã|Â|Ầ|Ấ|Ậ|Ẩ|Ẫ|Ă|Ằ|Ắ|Ặ|Ẳ|Ẵ/g, "A");
+    str = str.replace(/È|É|Ẹ|Ẻ|Ẽ|Ê|Ề|Ế|Ệ|Ể|Ễ/g, "E");
+    str = str.replace(/Ì|Í|Ị|Ỉ|Ĩ/g, "I");
+    str = str.replace(/Ò|Ó|Ọ|Ỏ|Õ|Ô|Ồ|Ố|Ộ|Ổ|Ỗ|Ơ|Ờ|Ớ|Ợ|Ở|Ỡ/g, "O");
+    str = str.replace(/Ù|Ú|Ụ|Ủ|Ũ|Ư|Ừ|Ứ|Ự|Ử|Ữ/g, "U");
+    str = str.replace(/Ỳ|Ý|Ỵ|Ỷ|Ỹ/g, "Y");
+    str = str.replace(/Đ/g, "D");
+    // Some system encode vietnamese combining accent as individual utf-8 characters
+    // Một vài bộ encode coi các dấu mũ, dấu chữ như một kí tự riêng biệt nên thêm hai dòng này
+    str = str.replace(/\u0300|\u0301|\u0303|\u0309|\u0323/g, ""); // ̀ ́ ̃ ̉ ̣  huyền, sắc, ngã, hỏi, nặng
+    str = str.replace(/\u02C6|\u0306|\u031B/g, ""); // ˆ ̆ ̛  Â, Ê, Ă, Ơ, Ư
+    // Remove extra spaces
+    // Bỏ các khoảng trắng liền nhau
+    str = str.replace(/ + /g, " ");
+    str = str.trim();
+    // Remove punctuations
+    // Bỏ dấu câu, kí tự đặc biệt
+    str = str.replace(
+      /!|@|%|\^|\*|\(|\)|\+|\=|\<|\>|\?|\/|,|\.|\:|\;|\'|\"|\&|\#|\[|\]|~|\$|_|`|-|{|}|\||\\/g,
+      " "
+    );
+    return str;
+  }
+
   const get_service = async () => {
     await axios
       .get(`/v1/service/list_service`)
@@ -163,7 +202,18 @@ function ServiceAdmin() {
             data_service.push(ob_service);
           });
 
-        setDataSource(data_service);
+        let new_arr = data_service.filter((item) => {
+          // Chuyển đổi tất cả các chuỗi có dấu sang không dấu
+          let word_Change_VN = removeVietnameseTones(item.service_name);
+          let word_search = removeVietnameseTones(search);
+
+          // Kiểm tra xem chuỗi đã được chuyển đổi có chứa từ khóa tìm kiếm hay không
+          return search.toLowerCase() === ""
+            ? item
+            : word_Change_VN.toLowerCase().includes(word_search);
+        });
+
+        setDataSource(new_arr);
       })
       .catch((e) => {
         console.log(e);
@@ -183,7 +233,9 @@ function ServiceAdmin() {
       .then(async (result) => {
         if (result.isConfirmed) {
           await axios
-            .patch(`/v1/service/update_one_field_service/${id_service}`, {status: !status} )
+            .patch(`/v1/service/update_one_field_service/${id_service}`, {
+              status: !status,
+            })
             .then((data) => {
               get_service();
               Swal.fire({
@@ -216,7 +268,7 @@ function ServiceAdmin() {
         });
         console.log(e);
       });
-  }
+  };
 
   useEffect(() => {
     get_service();
@@ -290,6 +342,27 @@ function ServiceAdmin() {
           <BottomCssContent>
             <TopCssContent>
               <p>Gói dịch vụ</p>
+              <div
+                className="d-flex"
+                style={{ width: "300px", borderRadius: "5px" }}
+              >
+                <input
+                  type="text"
+                  id="find_service"
+                  className="form-control form-control-lg"
+                  placeholder="Nhập vào tên dịch vụ..."
+                  style={{ fontSize: "17px", borderRadius: "3px" }}
+                  onChange={(e) => setSearch(e.target.value)}
+                />
+                <SearchOutlined
+                  style={{
+                    backgroundColor: "#7bd6e5",
+                    padding: "13px",
+                    color: "white",
+                    cursor: "pointer",
+                  }}
+                />
+              </div>
               <Link to="/admin/service/add">
                 <Button
                   style={{
--- frontend/src/Pages/Admin/PagesAdmin/Vehicle/VehicleAdmin.jsx
+++ frontend/src/Pages/Admin/PagesAdmin/Vehicle/VehicleAdmin.jsx
@@ -20,6 +20,7 @@ import {
   FolderViewOutlined,
   DeleteOutlined,
   SwapOutlined,
+  SearchOutlined,
 } from "@ant-design/icons";
 
 import axios from "axios";
@@ -135,6 +136,44 @@ function VehicleAdmin() {
     },
   ];
 
+  //Search Realtime
+  const [search, setSearch] = useState("");
+  useEffect(() => {
+    get_vehicle();
+  }, [search]);
+
+  function removeVietnameseTones(str) {
+    str = str.replace(/à|á|ạ|ả|ã|â|ầ|ấ|ậ|ẩ|ẫ|ă|ằ|ắ|ặ|ẳ|ẵ/g, "a");
+    str = str.replace(/è|é|ẹ|ẻ|ẽ|ê|ề|ế|ệ|ể|ễ/g, "e");
+    str = str.replace(/ì|í|ị|ỉ|ĩ/g, "i");
+    str = str.replace(/ò|ó|ọ|ỏ|õ|ô|ồ|ố|ộ|ổ|ỗ|ơ|ờ|ớ|ợ|ở|ỡ/g, "o");
+    str = str.replace(/ù|ú|ụ|ủ|ũ|ư|ừ|ứ|ự|ử|ữ/g, "u");
+    str = str.replace(/ỳ|ý|ỵ|ỷ|ỹ/g, "y");
+    str = str.replace(/đ/g, "d");
+    str = str.replace(/À|Á|Ạ|Ả|Ã|Â|Ầ|Ấ|Ậ|Ẩ|Ẫ|Ă|Ằ|Ắ|Ặ|Ẳ|Ẵ/g, "A");
+    str = str.replace(/È|É|Ẹ|Ẻ|Ẽ|Ê|Ề|Ế|Ệ|Ể|Ễ/g, "E");
+    str = str.replace(/Ì|Í|Ị|Ỉ|Ĩ/g, "I");
+    str = str.replace(/Ò|Ó|Ọ|Ỏ|Õ|Ô|Ồ|Ố|Ộ|Ổ|Ỗ|Ơ|Ờ|Ớ|Ợ|Ở|Ỡ/g, "O");
+    str = str.replace(/Ù|Ú|Ụ|Ủ|Ũ|Ư|Ừ|Ứ|Ự|Ử|Ữ/g, "U");
+    str = str.replace(/Ỳ|Ý|Ỵ|Ỷ|Ỹ/g, "Y");
+    str = str.replace(/Đ/g, "D");
+    // Some system encode vietnamese combining accent as individual utf-8 characters
+    // Một vài bộ encode coi các dấu mũ, dấu chữ như một kí tự riêng biệt nên thêm hai dòng này
+    str = str.replace(/\u0300|\u0301|\u0303|\u0309|\u0323/g, ""); // ̀ ́ ̃ ̉ ̣  huyền, sắc, ngã, hỏi, nặng
+    str = str.replace(/\u02C6|\u0306|\u031B/g, ""); // ˆ ̆ ̛  Â, Ê, Ă, Ơ, Ư
+    // Remove extra spaces
+    // Bỏ các khoảng trắng liền nhau
+    str = str.replace(/ + /g, " ");
+    str = str.trim();
+    // Remove punctuations
+    // Bỏ dấu câu, kí tự đặc biệt
+    str = str.replace(
+      /!|@|%|\^|\*|\(|\)|\+|\=|\<|\>|\?|\/|,|\.|\:|\;|\'|\"|\&|\#|\[|\]|~|\$|_|`|-|{|}|\||\\/g,
+      " "
+    );
+    return str;
+  }
+
   const get_vehicle = async () => {
     await axios
       .get(`/v1/vehicle/list_vehicle`)
@@ -158,7 +197,18 @@ function VehicleAdmin() {
             data_service.push(ob_service);
           });
 
-        setDataSource(data_service);
+        let new_arr = data_service.filter((item) => {
+          // Chuyển đổi tất cả các chuỗi có dấu sang không dấu
+          let word_Change_VN = removeVietnameseTones(item.vehicle_name);
+          let word_search = removeVietnameseTones(search);
+
+          // Kiểm tra xem chuỗi đã được chuyển đổi có chứa từ khóa tìm kiếm hay không
+          return search.toLowerCase() === ""
+            ? item
+            : word_Change_VN.toLowerCase().includes(word_search);
+        });
+
+        setDataSource(new_arr);
       })
       .catch((e) => {
         console.log(e);
@@ -319,21 +369,24 @@ function VehicleAdmin() {
       .get(`/v1/vehicle/moving_fee/${id.id}`)
       .then((data) => {
         let data_result = data.data.movingFee_id;
-        console.log(data_result)
+        console.log(data_result);
         const objectMoving = {
           name: data_result?.name,
-          priceFirst10km: data_result?.priceFirst10km.toLocaleString() +" đ",
-          priceFrom11to45: data_result?.priceFrom11to45.toLocaleString()+" đ",
-          pricePer45km: data_result?.pricePer45km.toLocaleString()+" đ",
-          waiting_fee: data_result?.waiting_fee.toLocaleString()+" đ",
-          TwowayFloor_loadingFee: data_result?.TwowayFloor_loadingFee.toLocaleString()+" đ",
-          OnewayFloor_loadingFee: data_result?.OnewayFloor_loadingFee.toLocaleString()+" đ",
-          Twoway_loadingFee: data_result?.Twoway_loadingFee.toLocaleString()+" đ",
-          Oneway_loadingFee: data_result?.Oneway_loadingFee.toLocaleString()+" đ",
+          priceFirst10km: data_result?.priceFirst10km.toLocaleString() + " đ",
+          priceFrom11to45: data_result?.priceFrom11to45.toLocaleString() + " đ",
+          pricePer45km: data_result?.pricePer45km.toLocaleString() + " đ",
+          waiting_fee: data_result?.waiting_fee.toLocaleString() + " đ",
+          TwowayFloor_loadingFee:
+            data_result?.TwowayFloor_loadingFee.toLocaleString() + " đ",
+          OnewayFloor_loadingFee:
+            data_result?.OnewayFloor_loadingFee.toLocaleString() + " đ",
+          Twoway_loadingFee:
+            data_result?.Twoway_loadingFee.toLocaleString() + " đ",
+          Oneway_loadingFee:
+            data_result?.Oneway_loadingFee.toLocaleString() + " đ",
         };
 
         const arrMoving = [objectMoving];
-
 
         Modal.success({
           title: "Thông tin phí di chuyển phương tiện",
@@ -370,6 +423,27 @@ function VehicleAdmin() {
           <BottomCssContent>
             <TopCssContent>
               <p>Phương tiện</p>
+              <div
+                className="d-flex"
+                style={{ width: "300px", borderRadius: "5px" }}
+              >
+                <input
+                  type="text"
+                  id="find_vehicle"
+                  className="form-control form-control-lg"
+                  placeholder="Nhập vào tên phương tiện..."
+                  style={{ fontSize: "17px", borderRadius: "3px" }}
+                  onChange={(e) => setSearch(e.target.value)}
+                />
+                <SearchOutlined
+                  style={{
+                    backgroundColor: "#7bd6e5",
+                    padding: "13px",
+                    color: "white",
+                    cursor: "pointer",
+                  }}
+                />
+              </div>
               <Link to="/admin/vehicle/add">
                 <Button
                   style={{
